2da401fc431a2a3cf3857d83eb5dd027244bacb9
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-03-19 10:52:45 +0530

    Add files via upload

---
+++ array/Ceiling.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ceiling {
+	public static int ceiling(int[] arr,int number) {
+		int start = 0;
+		int end = arr.length-1;
+		int mid=start+(end-start)/2;
+		while(start<=end) {
+			mid = start+(end-start)/2;
+			if(arr[mid]==number) {
+				return mid;
+				}
+			else if(arr[mid]<number)
+				start = mid+1;
+			else {
+				end= mid-1;
+				}
+		}
+		return start;
+	}
+	public static void main(String[] args) {
+		int[] arr = {2,5,7,9,12,23,42};
+		Arrays.sort(arr);
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a number : ");
+		int searchNumber = sc.nextInt();
+		System.out.println(ceiling(arr,searchNumber));
+		sc.close();
+	}
+}
