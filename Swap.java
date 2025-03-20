e6af9f474411c6faa17ac066c8c419169a971850
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-03-20 15:39:47 +0530

    Update Swap.java

--- Swap.java
+++ Swap.java
@@ -1,11 +1,6 @@
 class Swap{
 	public static void main(String args[]){
-		int a = 5;
-		int b = 25;
-		System.out.println("original a is "+a+" Original b is "+b);
-		int tmp = a;
-		a = b;
-		b = tmp;
-		System.out.println("Swapped a is "+a+" Swapped b is "+b);
+		for(int i;i<10;i++)
+		System.out.println(i);
 	}
 }
