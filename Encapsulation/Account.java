fd10b4651e0f6b06988c1825ec4ef59cf28e62e0
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-03-22 16:00:51 +0530

    Update Account.java

--- Encapsulation/Account.java
+++ Encapsulation/Account.java
@@ -1,5 +1,34 @@
 package Encapsulation;
 
 public class Account {
-
+	private String AccountHoldersName;
+	private double Balance;
+	private long AccountNumber;
+	
+	Account(String AccountHoldersName,double Balance,long AccountNumber){
+		this.AccountHoldersName = AccountHoldersName;
+		this.Balance = Balance;
+		this.AccountNumber = AccountNumber;
+		System.out.print("Account Created");
+	}
+	
+	public String setAccountHoldersName() {
+		return this.AccountHoldersName;
+	}
+	public double setBalance() {
+		return this.Balance;
+	}
+	public long setAccountNumber() {
+		return this.AccountNumber;
+	}
+	
+	public void getAccountHoldersName(String AccountHoldersName) {
+		this.AccountHoldersName = AccountHoldersName;
+	}
+	public void getBalance(double Balance) {
+		this.Balance = Balance;
+	}
+	public void getAccountNumber(long AccountNumber) {
+		this.AccountNumber = AccountNumber;
+	}
 }
