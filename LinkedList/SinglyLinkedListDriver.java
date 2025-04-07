a595842836d429c1279da1806539c20ef97eed15
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-04-07 14:18:12 +0530

    Update SinglyLinkedListDriver.java

--- LinkedList/SinglyLinkedListDriver.java
+++ LinkedList/SinglyLinkedListDriver.java
@@ -6,9 +6,17 @@ public class SinglyLinkedListDriver {
 		list.append(10);
 		list.append(20);
 		list.append(30);
+		list.display();
 		list.prepend(50);
 		list.prepend(60);
+		list.display();
 		list.insertAtIndex(100, 3);
+		list.display();
+		list.deleteAtEnd();
+		list.display();
+		list.deleteFromFirst();
+		list.display();
+		list.deleteAtIndex(2);
 		list.display();
 	}
 }
