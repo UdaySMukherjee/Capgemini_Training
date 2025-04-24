6e5f61974b69c53bda4fc7a5e87f097cf3380cee
UDAY SANKAR MUKHERJEE <111909529+UdaySMukherjee@users.noreply.github.com>
2025-04-24 10:45:46 +0530

    Add files via upload

---
+++ Annotation/Demo.java
@@ -0,0 +1,10 @@
+package annotations;
+
+public class Demo {
+	public static void main(String[] args) {
+		@SuppressWarnings("rawtypes")
+		ArrayList arr = new ArrayList();
+		
+		Integer i = new Integer(10);
+	}
+}
